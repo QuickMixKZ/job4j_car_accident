0e1a4f69b1be9e5eec02ad534cff3ed8d62b7a5c
QuickMixKZ <quickmixkz@gmail.com>
2022-08-29 11:17:58 +0500

    2. Регистрация пользователя [#296069]

--- src/main/java/ru/job4j/accident/Main.java
+++ src/main/java/ru/job4j/accident/Main.java
@@ -4,6 +4,7 @@ import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
